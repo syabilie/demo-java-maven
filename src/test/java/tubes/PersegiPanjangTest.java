f4631aa3380c1792d18c22c004752421be33424c
ngangong <selsa844@gmail.com>
2023-06-09 21:19:54 +0700

    Update PersegiPanjangTest.java

--- src/test/java/tubes/PersegiPanjangTest.java
+++ src/test/java/tubes/PersegiPanjangTest.java
@@ -6,7 +6,10 @@ import static org.junit.Assert.assertEquals;
 public class PersegiPanjangTest {
     @Test
     public void testHitungLuas() {
-        PersegiPanjang persegiPanjang = new PersegiPanjang(5, 3);
+        double panjang = 5;
+        double lebar = 3;
+
+        PersegiPanjang persegiPanjang = new PersegiPanjang(panjang, lebar);
         double luas = persegiPanjang.hitungLuas();
         assertEquals(15.0, luas, 0);
     }
